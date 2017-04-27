8a640ef4f626aedd355e2f6b65e0f6cdf21e3304
Curtis White <c-white@nf.sympatico.ca>
2017-04-27 19:56:32 -0230

    Add files via upload

--- assets/js/calculator-interface.js
+++ assets/js/calculator-interface.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-
+	
     /*
      * JQuery Interface Functions
      */
@@ -18,23 +18,11 @@ window.onload = function() {
             stack: ".draggable",
             cancel: false
         });
-        $(".resizable").resizable({
-            containment: "#container",
-            maxWidth: 1000,
-            minWidth: 300,
-            maxHeight: 100,
-            minHeight: 60,
-            handles: "se",
-            resize: function(event, ui) {
-
-            }
-        });
     });
-
+	
     /*
      * Toolbar Functions
      */
-
     //Snap Mode
     document.querySelector("#snap-toggle").addEventListener("click", function() {
         var snaptoggle = document.getElementById("snap-toggle");
@@ -72,41 +60,46 @@ window.onload = function() {
             snaptoggle.innerHTML = "snap on";
         }
     }, false);
-
-
+	
     /*
-    // Delete Mode
-    document.querySelector("#delete-mode-toggle").addEventListener("click",function() {
-    	var deletetoggle = document.getElementById("delete-mode-toggle");
-    	if (deletetoggle.innerHTML === "delete mode on") {
-    		
-    		$(".deletable").mouseup(function(){
-      // Clear timeout
-      return false;
-    }).mousedown(function(){
-      // Set timeout
-      return false; 
-    });
-    		
-              deletetoggle.innerHTML = "delete mode off";
-    	} else {
-    				//Delete On Long Press Function
-    var pressTimer;
-
-    $(".deletable").mouseup(function(){
-      clearTimeout(pressTimer);
-      // Clear timeout
-      return false;
-    }).mousedown(function(e){
-      // Set timeout
-      pressTimer = window.setTimeout(function() { $(e.target).remove();},1000);
-      return false; 
-    });
-              deletetoggle.innerHTML = "delete mode on";
-    	}
-        },false);
-    */
-
+     * Interact JS Functions
+     * Interact is required for resizing as an issue in jQueryUI causes elements to fall out of alignment.
+     * This issue is not present in Interact.
+     */
+    // Resizing
+    interact('.resizable')
+        .resizable({
+            preserveAspectRatio: false,
+            edges: {
+                left: true,
+                right: true,
+                bottom: true,
+                top: true
+            }
+        })
+        .on('resizemove', function(event) {
+            var target = event.target,
+                x = (parseFloat(target.getAttribute('data-x')) || 0),
+                y = (parseFloat(target.getAttribute('data-y')) || 0);
+            // update the element's style
+            target.style.width = event.rect.width + 'px';
+            target.style.height = event.rect.height + 'px';
+            // translate when resizing from top or left edges
+            x += event.deltaRect.left;
+            y += event.deltaRect.top;
+            target.style.webkitTransform = target.style.transform =
+                'translate(' + x + 'px,' + y + 'px)';
+            target.setAttribute('data-x', x);
+            target.setAttribute('data-y', y);
+        });
+		
+    // Delete
+    interact('.deletable')
+        .on('hold', function(event) {
+            $(event.target).remove();
+            event.preventDefault();
+        });
+		
     /*
      * Calculator Functions
      */
@@ -117,83 +110,55 @@ window.onload = function() {
         zero,
         period,
         operator;
-
+		
     screen = document.getElementById("result");
-
+	
     var elem = document.querySelectorAll(".num");
-
     var len = elem.length;
-
     for (var i = 0; i < len; i++) {
-
         elem[i].addEventListener("click", function() {
-
             num = this.value;
-
             if (screen.innerHTML === "undefined" || screen.innerHTML === "0") {
                 screen.innerHTML = "";
             }
             output = screen.innerHTML += num;
-
             limit = output.length;
-
             if (limit > 16) {
-
                 alert("Sorry no more input is allowed");
-
             }
-
         }, false);
-
     }
-
+	
     document.querySelector(".zero").addEventListener("click", function() {
-
         zero = this.value;
-
         if (screen.innerHTML === "") {
-
             output = screen.innerHTML = zero;
         } else if (screen.innerHTML === output) {
-
             output = screen.innerHTML += zero;
-
         }
-
     }, false);
-
+	
     document.querySelector(".period").addEventListener("click", function() {
-
         period = this.value;
-
         if (screen.innerHTML === "") {
-
             output = screen.innerHTML = screen.innerHTML.concat("0.");
-
         } else if (screen.innerHTML === output) {
-
             screen.innerHTML = screen.innerHTML.concat(".");
-
         }
-
     }, false);
-
-
+	
     document.querySelector("#eqn-bg").addEventListener("click", function() {
-
         if (screen.innerHTML === "") {
             screen.innerHTML = "0";
             output = screen.innerHTML;
         } else if (screen.innerHTML === output) {
-
             screen.innerHTML = eval(output);
             output = screen.innerHTML;
         } else {
             screen.innerHTML = eval(output);
         }
-
     }, false);
-
+	
     document.querySelector("#delete").addEventListener("click", function() {
         if (screen.innerHTML === "undefined" || screen.innerHTML === "") {
             screen.innerHTML = "";
@@ -202,31 +167,18 @@ window.onload = function() {
             screen.innerHTML = output.substr(0, output.length - 1);
             output = output.substr(0, output.length - 1);
         }
-
     }, false);
-
-
+	
     var elem1 = document.querySelectorAll(".operator");
-
     var len1 = elem1.length;
-
     for (var i = 0; i < len1; i++) {
-
         elem1[i].addEventListener("click", function() {
-
             operator = this.value;
-
             if (screen.innerHTML === "") {
-
                 screen.innerHTML = screen.innerHTML.concat("");
-
             } else if (output) {
-
                 screen.innerHTML = output.concat(operator);
-
             }
-
         }, false);
-
     }
 }
